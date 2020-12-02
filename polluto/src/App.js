4cf0219f0d1c40a42405f314fb9777195e803d38
Rita <rita@ritacorreia.co>
2020-12-02 15:25:37 +0000

    Added google-map-react, logo nav, retrieves user's location with use-position

--- polluto/src/App.js
+++ polluto/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import GoogleMapReact from "google-map-react";
+import { usePosition } from "use-position";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		// fetch from the API
+		// store the result in data via setData()
+	}, []);
+
+	return (
+		<div>
+			<h1 className="polluto-logo">polluto.</h1>
+
+			<SimpleMap />
+		</div>
+	);
 }
+
+const Marker = ({ text }) => <div>{text}</div>;
+
+const SimpleMap = () => {
+	const { latitude, longitude, error } = usePosition();
+
+	if (error !== null) {
+		return <div>Error getting your current location</div>;
+	}
+
+	if (!latitude || !longitude) {
+		return <div>Loading...</div>;
+	}
+
+	const center = {
+		lat: latitude,
+		lng: longitude,
+	};
+
+	return (
+		<div style={{ height: "90vh", width: "100%" }}>
+			<GoogleMapReact
+				bootstrapURLKeys={{
+					key: "AIzaSyA0ilaCkaAdHbwnSicLrPX9AqRRJZjMWAg",
+				}}
+				defaultCenter={center}
+				defaultZoom={14}
+			>
+				<Marker lat={center.lat} lng={center.lng} text="My Marker" />
+			</GoogleMapReact>
+		</div>
+	);
+};
 
 export default App;
