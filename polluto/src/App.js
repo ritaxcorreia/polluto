608da337e2a98f41ef3321804ee88759bcd633f7
Rita <rita@ritacorreia.co>
2020-12-03 18:37:42 +0000

    Update App.js

--- polluto/src/App.js
+++ polluto/src/App.js
@@ -5,13 +5,6 @@ import { usePosition } from "use-position";
 import Footer from "./components/Footer";
 
 function App() {
-	// const [data, setData] = useState([]);
-
-	// useEffect(() => {
-	// 	// fetch from the API
-	// 	// store the result in data via setData()
-	// }, []);
-
 	return (
 		<div>
 			<h1 className="polluto-logo">polluto.</h1>
@@ -23,10 +16,33 @@ function App() {
 	);
 }
 
-const Marker = ({ text }) => <div>{text}</div>;
-
 const SimpleMap = () => {
 	const { latitude, longitude, error } = usePosition();
+
+	console.log(latitude);
+	console.log(longitude);
+
+	const apiUrl = `https://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=4a6dd48a-819d-4f42-943c-e84587a03a4f`;
+
+	// const [showResults, setShowResults] = React.useState(false);
+	// const onChange = () => setShowResults(true);
+
+	let Card;
+
+	// Fetches data from IQAir Air Visual Air Pollution API
+	useEffect(() => {
+		fetch(apiUrl)
+			.then((response) => response.json())
+			.then(function (data) {
+				let city = data.city;
+				let state = data.state;
+				let country = data.country;
+				let { ts, aqius, mainus } = data.current.pollution;
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
 
 	if (error !== null) {
 		return <div>Error getting your current location</div>;
@@ -40,10 +56,18 @@ const SimpleMap = () => {
 		);
 	}
 
+	// Finds center by using user's own location coords (lat, lng)
 	const center = {
 		lat: latitude,
 		lng: longitude,
 	};
+
+	Card = ({ title, city, state, country, ts, aqius, mainus }) => (
+		<div className="card-container">
+			<h2 className="card-title">{title}</h2>
+			<h3 className="city-text">{city}</h3>
+		</div>
+	);
 
 	return (
 		<div style={{ height: "90vh", width: "100%" }}>
@@ -53,8 +77,12 @@ const SimpleMap = () => {
 				}}
 				defaultCenter={center}
 				defaultZoom={12}
+				onChange={({ center, zoom }) => {
+					console.log(center);
+					console.log(zoom);
+				}}
 			>
-				<Marker lat={center.lat} lng={center.lng} text="My Marker" />
+				<Card lat={center.lat} lng={center.lng} title="Air Pollution" />
 			</GoogleMapReact>
 		</div>
 	);
